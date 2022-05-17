2034c7c826a689c1e4241448bc1df4ae93596aaa
hangunhee94 <webzen9422@gmail.com>
2022-05-17 18:37:20 +0900

    1주차 과제

--- app.py
+++ app.py
@@ -1,23 +1,37 @@
+import hashlib
 import json
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)
+db = client.turtlegram
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"*": {"origins": "*"}})
 
 
-@app.route("/")
+@ app.route("/")
 def hello_world():
     return jsonify({'message': 'success'})
 
 
-@app.route("/signup", methods=["POST"])
+@ app.route("/signup", methods=["POST"])
 def sign_up():
     data = json.loads(request.data)
     print(data.get('email'))
     print(data["password"])
 
-    return jsonify({'message': 'success2'})
+    pw = data.get('email'),
+    hashed_password = hashlib.sha256(pw.encode('utf-8')).hexdigest()
+
+    doc = {
+        'email': data.get('email'),
+        'password': hashed_password
+    }
+    user = db.users.insert_one(doc)
+
+    return jsonify({'message': 'success', 'msg': '회원가입 완료!'})
 
 
 if __name__ == '__main__':
